21afc26671679a6f2124d1e175752e5fa1ce3d92
Korkodinova <berezkina140596@gmail.com>
2020-12-17 19:20:51 +0300

    функция push

--- 1.py
+++ 1.py
@@ -3,16 +3,15 @@ My little Stack
 """
 from typing import Any
 
-
 my_stack = []
 
 def push(elem: Any) -> None:
     """
     Operation that add element to stack
-
     :param elem: element to be pushed
     :return: Nothing
     """
+
     my_stack.append(elem)
     return None
 
@@ -20,33 +19,33 @@ def push(elem: Any) -> None:
 def pop() -> Any:
     """
     Pop element from the top of the stack. If not elements - should return None.
-
     :return: popped element
     """
 
-    return None
+    if my_stack:
+        return my_stack.pop(0)
+    else:
+        return None
 
 
 def peek(ind: int = 0) -> Any:
     """
     Allow you to see at the element in the stack without popping it.
-
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
-    print(ind)
-    return None
+
+    return None if ind >= len (my_stack) else my_stack[-1 - ind]
 
 
 def clear() -> None:
     """
     Clear my stack
-
     :return: None
     """
+    my_stack.clear()
     return None
 
 
 if __name__ == '__main__':
-    push(1)
-    print(my_stack)
+
