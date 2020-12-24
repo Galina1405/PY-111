ac643c58908368493d37d016afdde2fc6508ef27
Korkodinova <berezkina140596@gmail.com>
2020-12-24 19:12:28 +0300

    функция sinx

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -4,6 +4,8 @@ Taylor series
 from typing import Union
 
 
+DELTA = 0.0001
+
 def ex(x: Union[int, float]) -> float:
     """
     Calculate value of e^x with Taylor series
@@ -22,5 +24,12 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    def item(n):
+        return pow(-1, n) / factorial(2*n+1) * pow(x, 2*n+1)
+
+    sum_ = 0
+    for i in count():
+        current_value = item(i)
+        sum_ += current_value
+        if abs(current_value) <= DELTA:
+            return sum_
