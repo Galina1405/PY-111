2d1c5c4005bb55173665efab0277b47025590532
Korkodinova <berezkina140596@gmail.com>
2021-01-28 20:36:38 +0300

    1 tasks

---
+++ offset_py111/offset_1.py
@@ -0,0 +1,9 @@
+# 1. Оценить асимптотическую сложность приведенного ниже алгоритма:
+# a = len(arr) - 1            O(1)
+# # out = list()              O(1)
+# # while a > 0:              O(logN)
+# #     out.append(arr[a])    O(N)
+# #     a = a // 1.7          O(1)
+# # out.merge_sort()          O(NlogN)
+
+# O(NlogN)
