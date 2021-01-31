7e4aee2fb0fb5a7d7424eab71488fb4cd492f9c5
Korkodinova <berezkina140596@gmail.com>
2021-01-31 22:09:15 +0300

    пересчитала асимптотическую сложность

--- offset_py111/offset_1.py
+++ offset_py111/offset_1.py
@@ -1,9 +1,9 @@
 # 1. Оценить асимптотическую сложность приведенного ниже алгоритма:
 # a = len(arr) - 1            O(1)
 # # out = list()              O(1)
-# # while a > 0:              O(logN)
-# #     out.append(arr[a])    O(N)
+# # while a > 0:              O(loga)
+# #     out.append(arr[a])    O(a)
 # #     a = a // 1.7          O(1)
-# # out.merge_sort()          O(NlogN)
+# # out.merge_sort()          O(logalogloga)
 
-# O(NlogN)
+# O(logalogloga)
